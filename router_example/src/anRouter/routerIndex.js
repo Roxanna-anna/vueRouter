7ec785f1c5bd33d909ea8675552e89b82c336535
anna <ethen@ethendeMacBook-Air.local>
2020-01-29 23:39:44 +0800

    u

---
+++ router_example/src/anRouter/routerIndex.js
@@ -0,0 +1,62 @@
+class HistoryRoute {
+  constructor () {
+    this.current = null
+  }
+}
+
+class anRouter {
+  constructor (options) {
+    this.mode = options.mode || 'hash'
+    this.anRoutes = options.anRoutes || []
+    this.history = new HistoryRoute()
+    this.routesMap = this.createMap(this.routes)
+    this.init()
+  }
+  createMap (routes) {
+    return routes.reduce((memo, current) => {
+      memo[current.path] = current.component
+      return memo
+    }, {})
+  }
+  init () {
+    if (this.mode == 'hash') {
+      location.hash ? '' : location.hash = '/'
+      window.addEventListener('load', () => {
+        this.history.current = location.hash.slice(1)
+      })
+      window.addEventListener('hashChange', () => {
+        this.history.current = location.hash.slice(1)
+      })
+    } else {
+      // history模式下
+      location.pathname ? '' : location.pathname = '/'
+      window.addEventListener('load', () => {
+        this.history.current = location.pathname
+      })
+      window.addEventListener('popstate', () => {
+        this.history.current = location.pathname
+      })
+    }
+  }
+}
+anRouter.install = function (vue) {
+  vue.mixin({
+    beforeCreate () {
+      if (this.$options && this.$options.router) {
+        this._root = this
+        this._router = this.$options.router
+      } else {
+        this._root = this.$parent._root
+      }
+      vue.util.defineReactive(this, 'current', this._router.history)
+    }
+  })
+  vue.component('router-view', {
+    render (h) {
+      let current = this._self._root._router.history.current
+      let routesMap = this._self._root._router.routesMap
+      return h(routesMap[current])
+    }
+  })
+}
+export default anRouter
--- router_example/src/main.js
+++ router_example/src/main.js
@@ -1,10 +1,53 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './anRouter/routerIndex'
+// import router from './router'
 import store from './store'
+import HelloWorld from './components/HelloWorld.vue'
 
 Vue.config.productionTip = false
 
+//   *************************************
+// 1.全局混入
+// 2.单元测试 extend
+
+var test = {
+  testa: 1
+}
+function a () {
+  console.log('in')
+}
+setTimeout(function () {
+  test.testa = 44444
+}, 4000)
+a.install = function (vue) {
+  // 全局混入VUE实例
+  console.log(vue.util.defineReactive)
+  vue.util.defineReactive(test, 'testa')
+  // 2 Vue.extend 扩展vue,会返回VUE文件的构造类
+  const Constructor = Vue.extend(HelloWorld)
+  const vm = new Constructor().$mount()
+  console.log(vm)
+
+  console.log(vue.util.extend)
+  console.log(vue.extend)
+
+  vue.mixin({
+    data () {
+      return {
+        c: 'this is mixin'
+      }
+    },
+    beforeCreate () {
+      this.test = test// 将test 注入
+    },
+    methods: {
+
+    }
+  })
+}
+Vue.use(a)
+//    *************************************
 new Vue({
   router,
   store,
